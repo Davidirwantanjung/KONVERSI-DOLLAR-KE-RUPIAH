cd8566d14ecbf97dd83a5e513479c31c71e2dabc
David irwan tanjung <45660042+Davidirwantanjung@users.noreply.github.com>
2018-12-18 18:44:09 +0700

    Add files via upload

---
+++ Konversi Dollar ke Rupiah.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+
+long rupiah=14500,dollar,kurs;
+printf("PROGRAM KONVERSI DOLLAR KE RUPIAH\n");
+printf("BY DAVID IRWAN TANJUNG NIM 311810336\n");
+printf("INSTITUT PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+printf("Masukkan nilai Dollar $USD : ");scanf("%d", &dollar);
+kurs=rupiah*dollar;
+printf("kurs =  %i * %i = Rp %i",dollar,rupiah,kurs);
+getch();
+}
